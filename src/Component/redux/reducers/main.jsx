e89576b28c379270ac6130e3be6705589c2d1647
Shreenath Panchal <shree.d.panchal1999@gmail.com>
2024-12-09 15:29:03 +0530

    Frontend

---
+++ src/Component/redux/reducers/main.jsx
@@ -0,0 +1,10 @@
+import { getProductsReducers } from "./Productsreducer";
+
+import {combineReducers} from "redux";
+
+const rootreducers = combineReducers({
+    getproductsdata : getProductsReducers,
+});
+
+export default rootreducers;
+
